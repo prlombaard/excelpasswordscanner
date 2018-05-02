09c2778479c2c3e8de758768fcead285ea9d63f9
Rudolph Lombaard <prlombaard@gmail.com>
2018-05-02 16:05:49 +0200

    Create gogetit.py
    Initial commit

---
+++ gogetit.py
@@ -0,0 +1,12 @@
+# gogetit.py
+
+# Given a filename that point to a excelfile on disk. Try to get the password for the Excel file.
+# Use either a wordlist or generate a sequence of letters / numbers
+
+
+def main():
+    print("main")
+
+
+if "__name__" == "__main__":
+    main()
