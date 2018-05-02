a8d672e5825e20415daec956227c59416df887ab
Rudolph Lombaard <prlombaard@gmail.com>
2018-05-02 16:13:17 +0200

    Update gogetit.py
    Added zip password scanner. 
    TODO: Add evil.zip that is password protected

--- gogetit.py
+++ gogetit.py
@@ -3,6 +3,21 @@
 # Given a filename that point to a excelfile on disk. Try to get the password for the Excel file.
 # Use either a wordlist or generate a sequence of letters / numbers
 
+import zipfile
+
+def scan_zip_passwords(filename='evil.zip'):
+    zFile = zipfile.ZipFile(filename)
+    with open(zFile) as passFile:
+        for line in passFile.readlines():
+            password = line.strip('\n')
+            try:
+                zFile.extractall(pwd=password)
+                print(f'[+] Password = {password}')
+                exit(0)
+            except Exception, e:
+                print(e)
+
+
 
 def main():
     print("main")
